d4cdb6a86396a90fb935674ba05201e8267d114d
hehonglu123 <hehonglu123@126.com>
2021-04-01 23:11:54 -0400

    status overwritten

--- stretch_service.py
+++ stretch_service.py
@@ -2,8 +2,14 @@ import RobotRaconteur as RR
 RRN=RR.RobotRaconteurNode.s
 import time, copy
 from stretch_body.robot import Robot
+from stretch_body.base import Base
+from stretch_body.arm import Arm
+from stretch_body.lift import Lift
+from stretch_body.pimu import Pimu
+from stretch_body.head import Head
+from stretch_body.wacc import Wacc
 
-###
+
 #### python2.7 RR depreciate
 # # 1. set_ reserved, need overwritten
 ####base
@@ -19,10 +25,218 @@ from stretch_body.robot import Robot
 # # 3. RR request timeout for home()
 # # 4. ALL status dict needs overwritten
 
+class arm_RR(Arm)
+	def __init__(self):
+		Arm.__init__(self)
+		self.status_rr= {'pos': 0.0, 'vel': 0.0, 'force':0.0,'timestamp_pc':0}
+	#status overwrite, no motor
+	def pull_status(self):
+		self.motor.pull_status()
+		self.status['timestamp_pc']=time.time()
+		self.status['pos']= self.motor_rad_to_translate(self.status['motor']['pos'])
+		self.status['vel'] = self.motor_rad_to_translate(self.status['motor']['vel'])
+		self.status['force'] = self.motor_current_to_translate_force(self.status['motor']['current'])
+		self.status_rr=copy.deepcopy(self.status)
+		self.status_rr.pop('motor', None)
+
+class base_RR(Base):
+	def __init__(self):
+		Base.__init__(self)
+		self.status_rr= {'timestamp_pc':0,'x':0,'y':0,'theta':0,'x_vel':0,'y_vel':0,'theta_vel':0, 'pose_time_s':0,'effort': [0, 0]}
+	#status overwrite, no wheel
+	def pull_status(self):
+		"""
+		Computes base odometery based on stepper positions / velocities
+		"""
+		self.left_wheel.pull_status()
+		self.right_wheel.pull_status()
+		self.status['timestamp_pc'] = time.time()
+
+		p0 = self.status['left_wheel']['pos']
+		p1 = self.status['right_wheel']['pos']
+		v0 = self.status['left_wheel']['vel']
+		v1 = self.status['right_wheel']['vel']
+		e0 = self.status['left_wheel']['effort']
+		e1 = self.status['right_wheel']['effort']
+		t0 = self.status['left_wheel']['timestamp']
+		t1 = self.status['right_wheel']['timestamp']
+		self.status['translation_force'] = self.motor_current_to_translation_force(self.left_wheel.status['current'],self.right_wheel.status['current'])
+		self.status['rotation_torque'] = self.motor_current_to_rotation_torque(self.left_wheel.status['current'],self.right_wheel.status['current'])
+
+		if self.first_step:
+			# Upon the first step, simply set the initial pose, since
+			# no movement has yet been recorded.
+			self.first_step = False
+
+			self.p0 = p0
+			self.p1 = p1
+
+			self.t0_s = t0
+			self.t1_s = t1
+
+			self.status['x'] = 0.0
+			self.status['y'] = 0.0
+			self.status['theta'] = 0.0
+
+			self.status['x_vel'] = 0.0
+			self.status['y_vel'] = 0.0
+			self.status['theta_vel'] = 0.0
+
+		else:
+			######################################################
+			# The odometry related was wrtten starting on January 14,
+			# 2019 whle looking at the following BSD-3-Clause licensed
+			# code for reference.
+
+			# https://github.com/merose/diff_drive/blob/master/src/diff_drive/odometry.py
+
+			# The code uses standard calculations. The reference code
+			# specifically cites a document with the following link,
+			# which appears to be broken.
+
+			# https://chess.eecs.berkeley.edu/eecs149/documentation/differentialDrive.pdf
+
+			# There are many sources on the internet for these
+			# equations. For example, the following document is
+			# helpful:
+
+			# http://www8.cs.umu.se/kurser/5DV122/HT13/material/Hellstrom-ForwardKinematics.pdf
+
+			prev_t0_s = self.t0_s
+			prev_t1_s = self.t1_s
+			t0_s = t0
+			t1_s = t1
+
+			delta_t0_s = t0_s - prev_t0_s
+			delta_t1_s = t1_s - prev_t1_s
+
+			if (delta_t0_s > 0.0) and (delta_t1_s > 0.0):
+				# update if time has passed for both motor readings, otherwise do nothing
+
+				average_delta_t_s = (delta_t0_s + delta_t1_s) / 2.0
+
+				# update the times, since both motors have new readings
+				self.prev_t0_s = self.t0_s
+				self.prev_t1_s = self.t1_s
+				self.t0_s = t0_s
+				self.t1_s = t1_s
+
+				# need to check on wrap around / rollover for wheel positions
+				self.prev_p0 = self.p0
+				self.prev_p1 = self.p1
+				self.p0 = p0
+				self.p1 = p1
+
+				# Transform the wheel rotations so that left and right
+				# wheel distances in meters have the convention that
+				# positive values for each wheel corresponds with forward
+				# motion of the mobile base.
+				prev_left_m = self.prev_p0 * self.meters_per_motor_rad
+				left_m = self.p0 * self.meters_per_motor_rad
+
+				prev_right_m = self.prev_p1 * self.meters_per_motor_rad
+				right_m = self.p1 * self.meters_per_motor_rad
+
+				delta_left_m = left_m - prev_left_m
+				delta_right_m = right_m - prev_right_m
+
+				delta_travel = (delta_right_m + delta_left_m) / 2.0
+				delta_theta = (delta_right_m - delta_left_m) / self.wheel_separation_m
+
+				prev_x = self.status['x']
+				prev_y = self.status['y']
+				prev_theta = self.status['theta']
+
+				if delta_left_m == delta_right_m:
+					# delta_theta is 0.0, which would result in a divide
+					# by zero error and corresponds with an infinite
+					# radius of curvature (0 curvature).
+					delta_x = delta_travel * cos(prev_theta)
+					delta_y = delta_travel * sin(prev_theta)
+				else:
+					# calculate the instantaneous center of curvature (ICC)
+					icc_radius = delta_travel / delta_theta
+					icc_x = prev_x - (icc_radius * sin(prev_theta))
+					icc_y = prev_y + (icc_radius * cos(prev_theta))
+
+					# calculate the change in position based on the ICC
+					delta_x = ((cos(delta_theta) * (prev_x - icc_x))
+							   - (sin(delta_theta) * (prev_y - icc_y))
+							   + icc_x - prev_x)
+
+					delta_y = ((sin(delta_theta) * (prev_x - icc_x))
+							   + (cos(delta_theta) * (prev_y - icc_y))
+							   + icc_y - prev_y)
+
+				# update the estimated total time passed since odometry started
+				self.status['pose_time_s'] = self.status['pose_time_s'] + average_delta_t_s
+
+				# update the robot's velocity estimates
+				self.status['x_vel'] = delta_travel / average_delta_t_s
+				self.status['y_vel'] = 0.0
+				self.status['theta_vel'] = delta_theta / average_delta_t_s
+
+				# update the robot's pose estimates
+				self.status['x'] = prev_x + delta_x
+				self.status['y'] = prev_y + delta_y
+				self.status['theta'] = (prev_theta + delta_theta) % (2.0 * pi)
+
+		self.status_rr=copy.deepcopy(self.status)
+		self.status_rr.pop('left_wheel', None)
+		self.status_rr.pop('right_wheel', None)
+
+	def rr_set_translate_velocity(self, v_m):
+		self.set_translate_velocity(v_m)
+	def rr_set_rotational_velocity(self, v_r):
+		self.rr_set_rotational_velocity(v_r)
+	def rr_set_velocity(self, v_m, w_r, a=None):
+		self.rr_set_velocity(v_m, w_r)
+
+class wacc_RR(Wacc):
+	def __init__(self):
+		Wacc.__init__(self)
+		self.status_rr= { 'ax':0,'ay':0,'az':0,'a0':0,'d0':0,'d1':0, 'd2':0,'d3':0,'single_tap_count': 0, 'state':0, 'debug':0,
+					   'timestamp': 0}
+	#status overwrite, no wheel
+	def unpack_status(self,s):
+		with self.lock:
+			sidx=0
+			if self.ext_status_cb is not None:
+				sidx+=self.ext_status_cb(s[sidx:])
+			self.status['ax'] = unpack_float_t(s[sidx:]);sidx+=4
+			self.status['ay'] = unpack_float_t(s[sidx:]);sidx+=4
+			self.status['az'] = unpack_float_t(s[sidx:]);sidx+=4
+			self.status['a0'] = unpack_int16_t(s[sidx:]);sidx+=2
+			self.status['d0'] = unpack_uint8_t(s[sidx:]); sidx += 1
+			self.status['d1'] = unpack_uint8_t(s[sidx:]); sidx += 1
+			self.status['d2'] = unpack_uint8_t(s[sidx:]); sidx += 1
+			self.status['d3'] = unpack_uint8_t(s[sidx:]); sidx += 1
+			self.status['single_tap_count'] = unpack_uint32_t(s[sidx:]);sidx += 4
+			self.status['state'] = unpack_uint32_t(s[sidx:]); sidx += 4
+			self.status['timestamp'] = self.timestamp.set(unpack_uint32_t(s[sidx:]));sidx += 4
+			self.status['debug'] = unpack_uint32_t(s[sidx:]);sidx += 4
+			
+			self.status_rr=copy.deepcopy(self.status)
+			self.status_rr.pop('transport', None)
+			return sidx
+	def rr_set_D2(self,on):#0 or 1
+		self.set_D2(on)
+
+	def rr_set_D3(self,on): #0 or 1
+		self.set_D3(on)
+
 class stretch_RR(Robot):
 	def __init__(self):
 		Robot.__init__(self)
-		self.arm
+		# self.pimu=Pimu_RR()
+		# self.head=Head_RR()
+
+		self.base=Base_RR()
+
+		self.lift=Lift_RR()
+
+		self.arm=Arm_RR()
+
 	def get_arm(self):
 		return self.arm, "edu.rpi.robotics.stretch.stretch_arm"
 	def get_base(self):
@@ -33,6 +247,8 @@ class stretch_RR(Robot):
 		return self.wacc, "edu.rpi.robotics.stretch.stretch_wacc"
 	def get_lift(self):
 		return self.lift, "edu.rpi.robotics.stretch.stretch_lift"
+
+
 
 def main():
 	robot=stretch_RR()
