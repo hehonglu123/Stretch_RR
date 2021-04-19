1ca9140988b486c80b1d8f5fcefa3e724d955f21
hehonglu123 <hehonglu123@126.com>
2021-04-19 16:14:12 -0400

    objref wire updates

--- stretch_client2.py
+++ stretch_client2.py
@@ -1,11 +1,13 @@
 
 from RobotRaconteur.Client import *     #import RR client library
-import sys, time
+import sys, time, traceback
 import numpy as np
+
+#url of each robot
 url1='rr+tcp://192.168.1.64:23232/?service=stretch'
 url2='rr+tcp://192.168.1.28:23232/?service=stretch'
 
-#Startup, connect, and pull out the camera from the objref    
+#Startup, connect, and pull out the arm/lift/eoa from the objref in robot obj  
 robot1=RRN.ConnectService(url1)
 robot2=RRN.ConnectService(url2)
 
@@ -29,6 +31,11 @@ end_of_arm2.move_to('stretch_gripper',25)		##(50 to -100)
 robot1.push_command()
 robot2.push_command()
 
+
+#Connect to arm status RR wire
+arm1_status=arm1.status_rr.Connect()
+arm2_status=arm2.status_rr.Connect()
+
 time.sleep(3)
 now=time.time()
 while True:
@@ -38,8 +45,7 @@ while True:
 		arm1.move_to(0.3+0.28*np.cos((time.time()-now)/2.))
 		end_of_arm1.move_to('wrist_yaw',np.sin((time.time()-now)/2.))
 		end_of_arm1.move_to('stretch_gripper',25+25*np.sin((time.time()-now)/2.))
-		print('arm height: '+str(lift1.status_rr['pos']))
-		print('arm length: '+str(arm1.status_rr['pos']))
+		
 
 		lift2.move_to(0.5+0.2*np.sin((time.time()-now)/2.))
 		arm2.move_to(0.3+0.28*np.cos((time.time()-now)/2.))
@@ -48,9 +54,14 @@ while True:
 
 		robot1.push_command()
 		robot2.push_command()
+		#example to read RR status wire
+		print('arm1 pos: '+str(arm1_status.InValue['pos']))
+		print('arm2 pos: '+str(arm2_status.InValue['pos']))
+		#timing test
 		print(time.time()-now2)
 		# time.sleep(0.02)
 	except:
+		traceback.print_exc()
 		break
 
 print ('Retracting...')
